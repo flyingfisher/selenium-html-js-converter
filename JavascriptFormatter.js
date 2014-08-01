5c4357df25d4002a1e591980f8dcc1c72d9d7cf5
fish <yu.flying1004@gmail.com>
2014-08-01 16:17:08 +0800

    fix small problem compile ts

--- JavascriptFormatter.js
+++ JavascriptFormatter.js
@@ -119,8 +119,7 @@ function setIndent(i) {
 app.postFilter = function (originalCommands) {
     var commands = [];
     var commandsToSkip = {
-        'waitForPageToLoad': 1,
-        'pause': 1
+        'waitForPageToLoad': 1
     };
     var rc;
     for (var i = 0; i < originalCommands.length; i++) {
