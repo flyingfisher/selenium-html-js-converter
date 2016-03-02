05db768203a511a82f4a3ca97aa1136d4c168ffd
Daniel Smedegaard Buus <danielbuus@gmail.com>
2016-03-02 15:12:10 +0100

    Hoist currendCommand var
    ... prepping for extensible commands stuf ...
    
    Signed-off-by: Daniel Smedegaard Buus <danielbuus@gmail.com>

--- JavascriptFormatter.ts
+++ JavascriptFormatter.ts
@@ -1284,12 +1284,11 @@ options.getHeader = function() {
   return '"use strict";\n'
     + "/* jslint node: true */\n\n"
     + "var assert = require('assert');\n\n"
-    + "var browser, element, options = { timeout: " + options.timeout + ", retries: " + options.retries + ", screenshotFolder: '" + options.screenshotFolder + "', lbParam: {vuSn: 1}, baseUrl: '" + options.baseUrl + "' };\n\n"
+    + "var browser, element, currentCommand = '', options = { timeout: " + options.timeout + ", retries: " + options.retries + ", screenshotFolder: '" + options.screenshotFolder + "', lbParam: {vuSn: 1}, baseUrl: '" + options.baseUrl + "' };\n\n"
     + "module.exports = function ${methodName} (_browser, _options)  {\n\n"
     + "browser = _browser;\n"
     + "var acceptNextAlert = true;\n"
     + "getRuntimeOptions(_options);\n"
-    + "var currentCommand = '';\n\n"
     + "try {\n";
 };
 
